70a7690351af168baf174f40c456c04d247d3656
김승현 <31381487+reiley-kim@users.noreply.github.com>
2024-01-30 17:20:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,9 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'requests',
-        'torchvision@git+https://github.com/reiley-kim/vision.git@main#egg=torchvision'
+        # 'torchvision@git+https://github.com/reiley-kim/vision.git@main#egg=torchvision'
+        'torchvision',
+        'torch@git+https://github.com/pytorch/pytorch.git@nightly#egg=torch',
         'pillow'
     ]
 )
